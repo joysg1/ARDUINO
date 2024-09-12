50fa2e5f9b58f5a0032d3cd438f7d2bd6a3eecce
joysg1 <joy.nelaton@utp.ac.pa>
2024-09-12 16:14:41 -0500

    Commit_27

--- practica_24_encendido_led_boton1.ino
+++ practica_24_encendido_led_boton1.ino
@@ -1,24 +1,27 @@
 int ledPIN = 9;
 int boton = 2;
-int estadoBotonAnterior = LOW;
-int estadoLed = LOW;
+boolean estadoBotonAnterior = LOW;
+boolean estadoLed = LOW;
 
 void setup()
 {
   pinMode(ledPIN, OUTPUT);
   pinMode(boton, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  int estadoBoton = digitalRead(boton);
+  boolean estadoBoton = digitalRead(boton);
   if (estadoBoton == HIGH && estadoBotonAnterior == LOW) {
     if (estadoLed == LOW) {
       digitalWrite(ledPIN, HIGH);
       estadoLed = HIGH;
+      Serial.println("** LED ENCENDIDO **");
     } else {
       digitalWrite(ledPIN, LOW);
       estadoLed = LOW;
+      Serial.println("** LED APAGADO **");
     }
   }
   estadoBotonAnterior = estadoBoton;
